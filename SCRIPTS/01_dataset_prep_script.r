ee71e41a95dd484a58617ce99f9ad224fc9eff76
lulepo99 <160399383+lulepo99@users.noreply.github.com>
2024-04-06 12:28:09 +0200

    Update 01_dataset_prep_script.r

--- SCRIPTS/01_dataset_prep_script.r
+++ SCRIPTS/01_dataset_prep_script.r
@@ -1,9 +1,9 @@
 #SCRIPT
 
-assign("df_with_tmb", a_00_data_clinical_sample)
-rm(a_00_data_clinical_sample)
-df_with_relapse <- a_00_data_clinical_patient
-rm(a_00_data_clinical_patient)
+assign("df_with_tmb", data_clinical_sample)
+rm(data_clinical_sample)
+df_with_relapse <- data_clinical_patient
+rm(data_clinical_patient)
 
 
 df_with_tmb <- df_with_tmb[-c(1986:2509),]
@@ -22,11 +22,11 @@ total2$RFS_STATUS <- factor(substr(total2$RFS_STATUS, 1, 1), levels = c("0","1")
 
 METABRIC_NEW <- total2[,c(1:31,694:696,32:693)]
 
-rm(total,total2,total3,df_with_relapse,df_with_tmb)
+rm(total,total2,df_with_relapse,df_with_tmb)
 
 METABRIC_NEW <- METABRIC_NEW[,-c(35:523)] 
 
-expr_transposed <- t(a_00_data_mrna_illumina_microarray_zscores_ref_diploid_samples)
+expr_transposed <- t(data_mrna_illumina_microarray_zscores_ref_diploid_samples)
 colnames(expr_transposed) <- expr_transposed[1,]
 expr_transposed <- expr_transposed[-c(1,2),]
 expr_transposed <- as.data.frame(expr_transposed)
@@ -36,7 +36,10 @@ total3 <- merge(METABRIC_NEW, expr_transposed, by ="patient_id")
 total3 <- total3[,c(1:34, 208:20810,35:207)]
 METABRIC_NEW_GENES <- total3
 
-rm(expr_transposed, total3, a_00_data_mrna_illumina_microarray_zscores_ref_diploid_samples, a_00_data_mutations)
+rm(expr_transposed, total3, data_mrna_illumina_microarray_zscores_ref_diploid_samples)
+
+
+
 
 
 
