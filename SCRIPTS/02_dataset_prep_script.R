f0eba32985a276f825cbe24588de188a77c3803a
lulepo99 <160399383+lulepo99@users.noreply.github.com>
2024-04-13 18:08:32 +0200

    Update of the name of two variables

--- SCRIPTS/02_dataset_prep_script.R
+++ SCRIPTS/02_dataset_prep_script.R
@@ -66,13 +66,13 @@ METABRIC_NEW_GENES <- METABRIC_NEW_GENES[,c(1:34, 20811:20812,35:20810)]
 
 # Deleting patients based on NA and "Died of Other Causes" value (as "prova" to be sure not to do a mess)
 
-prova_new <- METABRIC_NEW %>%
+METABRIC_SUBSET <- METABRIC_NEW %>%
   filter(death_from_cancer!="Died of Other Causes", death_from_cancer!="", 
          pam50_._claudin.low_subtype!="NC",
          er_status_measured_by_ihc!="", cellularity!="", !is.na(mutation_count), 
          !is.na(tumor_size), !is.na(neoplasm_histologic_grade))
 
-prova_new_genes <- METABRIC_NEW_GENES %>%
+METABRIC_SUBSET_GENES <- METABRIC_NEW_GENES %>%
   filter(death_from_cancer!="Died of Other Causes", death_from_cancer!="", 
          pam50_._claudin.low_subtype!="NC",
          er_status_measured_by_ihc!="", cellularity!="", !is.na(mutation_count), 
