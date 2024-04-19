feca29c5074ff9b8a049648af006b28d3bdea56d
marco-cominelli01 <cominellimarco8@gmail.com>
2024-04-19 07:55:23 +0200

    Revert "Merge branch 'main' of https://github.com/lulepo99/breast-cancer-survival-analysis"
    This reverts commit 6d29957f6bb6ef4d1ccfd6d8b5061bc5384e1050, reversing
    changes made to 93237f4450229999945793b356a3436f2df805e4.

--- SCRIPTS/02_dataset_prep_script.R
+++ SCRIPTS/02_dataset_prep_script.R
@@ -66,13 +66,13 @@ METABRIC_NEW_GENES <- METABRIC_NEW_GENES[,c(1:34, 20811:20812,35:20810)]
 
 # Deleting patients based on NA and "Died of Other Causes" value (as "prova" to be sure not to do a mess)
 
-METABRIC_SUBSET <- METABRIC_NEW %>%
+prova_new <- METABRIC_NEW %>%
   filter(death_from_cancer!="Died of Other Causes", death_from_cancer!="", 
          pam50_._claudin.low_subtype!="NC",
          er_status_measured_by_ihc!="", cellularity!="", !is.na(mutation_count), 
          !is.na(tumor_size), !is.na(neoplasm_histologic_grade))
 
-METABRIC_SUBSET_GENES <- METABRIC_NEW_GENES %>%
+prova_new_genes <- METABRIC_NEW_GENES %>%
   filter(death_from_cancer!="Died of Other Causes", death_from_cancer!="", 
          pam50_._claudin.low_subtype!="NC",
          er_status_measured_by_ihc!="", cellularity!="", !is.na(mutation_count), 
